1633f3c0969c267152eb2e60d1ea9f7fd7cf1086
Anel Danza <aneldanza23@gmail.com>
2025-02-04 16:43:13 -0500

    add styling to the layout div and hight to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
-    <div>
-      <NavBar />
+    <div className="w-screen h-screen bg-gradient-to-b from-gradient-from via-gradient-mid to-gradient-to text-primary-text relative">
+      <div className="h-24">
+        <NavBar />
+      </div>
       <Outlet />
     </div>
   );
